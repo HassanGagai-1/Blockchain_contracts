883886af40b5749ec259b97c64800a0f401c59d7
HassanGagai-1 <bcsbs2112320@szabist.pk>
2025-02-27 13:01:48 +0500

    HassanToken

--- contracts/lecture_02/erc20.sol
+++ contracts/lecture_02/erc20.sol
@@ -5,8 +5,8 @@ pragma solidity >=0.8.2 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract Mytoken is ERC20, ERC20Permit{
-    constructor() ERC20("MyToken","MTK") ERC20Permit("MyToken"){
+contract Hassan is ERC20, ERC20Permit{
+    constructor() ERC20("Hassan","HSN") ERC20Permit("Hassan"){
 
     }
 
